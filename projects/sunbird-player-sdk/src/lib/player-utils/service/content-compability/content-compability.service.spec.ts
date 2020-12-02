31e98d7b60099fc6cc791b5d7849d63fb1e93368
Sourav <sourav_d@techjoomla.com>
2020-12-02 16:31:12 +0530

    Issue #21153 feat: Adding content compatability service

--- projects/sunbird-player-sdk/src/lib/player-utils/service/content-compability/content-compability.service.spec.ts
+++ projects/sunbird-player-sdk/src/lib/player-utils/service/content-compability/content-compability.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ContentCompabilityService } from './content-compability.service';
+
 
 describe('ContentCompabilityService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
+  it('call checkContentCompatibility and get true', () => {
     const service: ContentCompabilityService = TestBed.get(ContentCompabilityService);
-    expect(service).toBeTruthy();
+    const result = service.checkContentCompatibility(4, 5);
+    expect(result).toBeTruthy();
+  });
+
+  it('call checkContentCompatibility and get false', () => {
+    const service: ContentCompabilityService = TestBed.get(ContentCompabilityService);
+    const result = service.checkContentCompatibility(5, 4);
+    expect(result).toBeFalsy();
   });
 });
