9a60d69638d0d9143a3c71c05cbacdcdbfad98ea
diptesh6501 <diptesh1993@gmail.com>
2020-12-03 13:03:17 +0530

    Issue #SB-21153 fix: content compatibilty service refactored(fixed test cases)

--- projects/sunbird-player-sdk/src/lib/player-utils/service/content-compability/content-compability.service.spec.ts
+++ projects/sunbird-player-sdk/src/lib/player-utils/service/content-compability/content-compability.service.spec.ts
@@ -5,10 +5,23 @@ import { ContentCompabilityService } from './content-compability.service';
 describe('ContentCompabilityService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('call checkContentCompatibility and get true', () => {
+  it('call checkContentCompatibility and get compatibility error', () => {
     const service: ContentCompabilityService = TestBed.get(ContentCompabilityService);
+    const compatibilityError = new Error();
     service.playerContentCompatibiltyLevel = 4;
     const result = service.checkContentCompatibility(3);
-    expect(result).toBeTruthy();
+    const err = new Error();
+    err.message = null;
+    expect(result).toEqual({ error: null, isCompitable: true });
+  });
+
+  it('call checkContentCompatibility and get compatibility error as null', () => {
+    const service: ContentCompabilityService = TestBed.get(ContentCompabilityService);
+    service.playerContentCompatibiltyLevel = 4;
+    const result = service.checkContentCompatibility(5);
+    const compatibilityError = new Error();
+    compatibilityError.message = `Player supports 4 but content compatibility is 5`;
+    compatibilityError.name = 'contentCompatibily';
+    expect(result).toEqual({ error: compatibilityError , isCompitable: false });
   });
 });
