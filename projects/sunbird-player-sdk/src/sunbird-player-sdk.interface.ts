a0888d07ac87b886d6f0b338494986be58d3f2d0
Vivek Kasture <vivek_kasture@techjoomla.com>
2021-12-13 13:46:18 +0530

    Issue #SB-27917 fix: Added Accessibility

--- projects/sunbird-player-sdk/src/sunbird-player-sdk.interface.ts
+++ projects/sunbird-player-sdk/src/sunbird-player-sdk.interface.ts
@@ -1,4 +1,17 @@
 export interface NextContent {
-    name: string
-    identifer?: string
+  name: string;
+  identifer?: string;
+}
+
+export interface ISideBarConfig {
+  showShare: boolean;
+  showDownload: boolean;
+  showReplay: boolean;
+  showExit: boolean;
+  showPrint: boolean;
+}
+
+export interface ISideBarEvent {
+  event: KeyboardEvent | MouseEvent;
+  type: string;
 }
